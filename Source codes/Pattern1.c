b3f088b3c8830a0feabe81452d926153d7ff1e59
Zeeshan Abbasi <48386803+TechieZee@users.noreply.github.com>
2019-10-16 23:05:15 +0530

    Create Pattern1.c
      11111
        2222
         333
          22
           1

---
+++ Source codes/Pattern1.c
@@ -0,0 +1,23 @@
+ /*11111
+   2222
+    333
+     22
+      1*/
+ 
+ #include<iostream>
+    using namespace std;
+    main()
+    { int i,j,k,n;
+    cout<<"enter the number:";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {for(j=1;j<i;j++)
+    {
+	cout<<" ";
+	}
+	for(k=1;k<=n-i+1;k++)
+	{
+	cout<<i;
+    }cout<<"\n";}
+    return(0);
+	}
