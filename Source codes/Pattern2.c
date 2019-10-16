af0d556d4e288f84fe78e2646b5867e0442de109
Zeeshan Abbasi <48386803+TechieZee@users.noreply.github.com>
2019-10-16 23:07:07 +0530

    Create Pattern2.c
    # STEP PATTERN
    
    enter the number of star rows:  5
    
    
    * *
    * *
    * * * *
    * * * *
    * * * * * *
    * * * * * *
    * * * * * * * *
    * * * * * * * *
    * * * * * * * * * *
    * * * * * * * * * *

---
+++ Source codes/Pattern2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main()
+{
+	int n,i,j,k,g,m=1;
+	printf("enter the number of star rows:  ");
+	scanf("%d",&n);
+	printf("\n\n");
+	for(i=1;i<=n;i++)
+	{
+	for(j=0;j<2;j++)
+	{ 
+	k=2*m;
+	for(g=0;g<k;g++)
+	{
+	printf("* ");
+		}	
+		printf("\n");
+} 
+   m++;
+}}
