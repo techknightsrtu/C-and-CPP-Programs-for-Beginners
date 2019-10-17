00a82777d2c53284b3f893d0a6e1fdfda5feb5cc
KratoSkills <56100874+KratoSkills@users.noreply.github.com>
2019-10-17 09:44:46 +0530

    Update DiamondPattern.c

--- Source codes/DiamondPattern.c
+++ Source codes/DiamondPattern.c
@@ -1,30 +1,18 @@
 #include<stdio.h>
-main()
-{
+main() {
 int i,j,k;
-for(i=0;i<=10;i++)
-{
+for(i=0;i<=10;i++) {
 	for(j=10;j>=i;j--)
-	{
-	printf(" ");
-	}
+	        printf(" ");
 	for(k=0;k<=i;k++)
-	{
-		printf("* ");
-	}
+                printf("*");
 	printf("\n");
 }
-for(i=9;i>=0;i--)
-{
+for(i=9;i>=0;i--) {
 	for(j=9;j>=i;j--)
-	{
 		printf(" ");
-	}
 	for(k=0;k<=i;k++)
-	{
 		printf(" *");
-	}
 	printf("\n");
 }
-
 }
