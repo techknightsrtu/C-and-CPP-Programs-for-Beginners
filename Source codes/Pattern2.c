1659fd81f367f2291b13432d5a616fbe4c9ec13e
KratoSkills <56100874+KratoSkills@users.noreply.github.com>
2019-10-17 09:56:24 +0530

    Update Pattern2.c

--- Source codes/Pattern2.c
+++ Source codes/Pattern2.c
@@ -1,20 +1,15 @@
 #include<stdio.h>
-main()
-{
+main() {
 	int n,i,j,k,g,m=1;
-	printf("enter the number of star rows:  ");
-	scanf("%d",&n);
+	printf("Enter the number of star rows: ");
+	scanf(" %d",&n);
 	printf("\n\n");
-	for(i=1;i<=n;i++)
-	{
-	for(j=0;j<2;j++)
-	{ 
-	k=2*m;
-	for(g=0;g<k;g++)
-	{
-	printf("* ");
-		}	
-		printf("\n");
-} 
-   m++;
-}}
+	for(i=1;i<=n;i++) {
+	    for(j=0;j<2;j++) 
+	        k=2*m;
+	    for(g=0;g<k;g++)
+	        printf("* ");	
+	    printf("\n");
+            m++;
+        }
+}
