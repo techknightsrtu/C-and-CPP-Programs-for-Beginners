547d4401b6797db9dfac1225381eb0940885edcb
yashsongara101 <36963821+yashsongara101@users.noreply.github.com>
2020-10-23 12:15:35 +0530

    Update BubbleSort.c
    Search Algorithm optimised

--- Source codes/BubbleSort.c
+++ Source codes/BubbleSort.c
@@ -2,23 +2,22 @@
 using namespace std;
 main()
 {
-int a[6],i,j,b;
-cout<<"enter the elements in the array\n";
-for(i=0;i<6;i++)
-{
-cin>>a[i];	
- }
- for(i=0;i<6;i++)
- { for(j=0;j<6;j++)
- { if(a[j]>a[j+1])
- { b=a[j+1];
- a[j+1]=a[j];
- a[j]=b;
- }}
- } cout<<"sorted array is\n";
- for(i=0;i<6;i++)
- {
-cout<<a[i]<<"\n";
- } 
- return(0);
+  int a[6],i,j,b;
+  cout<<"enter the elements in the array\n";
+  for(i=0;i<6;i++){
+    cin>>a[i];	
+  }
+  for(i=0;i<6;i++)
+  { for(j=0;j<6;j++)
+     { if(a[j]>a[j+1])
+        { b=a[j+1];
+          a[j+1]=a[j];
+          a[j]=b;
+        }}
+    } cout<<"sorted array is\n";
+   for(i=0;i<6;i++)
+   {
+      cout<<a[i]<<"\n";
+   } 
+   return(0);
  } 
